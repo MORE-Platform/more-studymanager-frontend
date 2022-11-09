ef76de0bc0398dd25b2c67c7afb9d04b7f6f2492
iaigner <isabella.aigner@redlink.at>
2022-11-09 15:17:08 +0100

    MORE-230: Implement Menü Bar and Footer

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,14 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+  import Footer from './components/shared/Footer.vue'
+  import Header from './components/shared/Header.vue'
+</script>
 
 <template>
+  <Header />
   <main>
     <router-view />
   </main>
+  <Footer />
 </template>
 
 <style></style>
