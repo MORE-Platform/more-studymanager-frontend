dbc7b556388323175621ad7abe1195eedd2a376a
iaigner <isabella.aigner@redlink.at>
2022-11-21 18:09:06 +0100

    MORE-367: Added List functionalities - missing: object JSON.stringify

--- src/components/InterventionsList.vue
+++ src/components/InterventionsList.vue
@@ -2,18 +2,18 @@
   import {ref, Ref, PropType} from 'vue'
   import {useInterventionsApi} from "../composable/useApi";
   import {Intervention, Observation, StudyGroup} from '../generated-sources/openapi';
-  import {MoreTableAction, MoreTableColumn, MoreTableFieldType, MoreTableRowActionResult, MoreTableChoice} from "../models/MoreTableModel";
+  import {MoreTableAction, MoreTableColumn, MoreTableFieldType, MoreTableRowActionResult, MoreTableChoice, MoreTableActionResult} from "../models/MoreTableModel";
   import ConfirmDialog from 'primevue/confirmdialog';
   import DynamicDialog from 'primevue/dynamicdialog';
   import MoreTable from '../components/shared/MoreTable.vue'
   import {AxiosResponse} from "axios";
-  //import {useDialog} from "primevue/usedialog";
-  //import InterventionsDialog from '../components/dialog/InterventionDialog.vue'
+  import {useDialog} from "primevue/usedialog";
+  import InterventionDialog from '../components/dialog/InterventionDialog.vue'
 
   const { interventionsApi } = useInterventionsApi();
 
   const interventionList: Ref<Intervention[]> = ref([])
-  //const dialog = useDialog()
+  const dialog = useDialog()
   //const interventionTypes: Ref<MoreTableActionOptions[]> = ref([])
 
   const props = defineProps({
@@ -38,8 +38,9 @@
   ]
 
   const rowActions: MoreTableAction[] = [
+    {id: 'clone', label: 'Clone'},
     { id:'delete', label:'Delete', icon:'pi pi-trash', confirm: {header: 'Delete Study',
-        message: 'Deletion of an observation can’t be revoked! Are you sure you want to delete following observation: ...'}
+        message: 'Deletion of an intervention can’t be revoked! Are you sure you want to delete following intervention: ...'}
     }
   ]
 
@@ -48,14 +49,15 @@
       interventionList.value = await interventionsApi.listInterventions(props.studyId)
         .then((response:AxiosResponse) => response.data)
     } catch (e) {
-      console.error('cannot list studies', e)
+      console.error('cannot list interventions of study ' + props.studyId, e)
     }
   }
 
   function execute(action: MoreTableRowActionResult<StudyGroup>) {
     switch (action.id) {
       case 'delete': return deleteIntervention(action.row)
-      //case 'create': return openInterventionDialog(action as MoreTableActionResult)
+      case 'create': return openInterventionDialog('Create Intervention')
+      case 'clone': return openInterventionDialog('Clone Intervention', action.row, 'clone')
       default: console.error('no handler for action', action)
     }
   }
@@ -63,19 +65,28 @@
   async function changeValue(intervention:Intervention) {
     try {
       //do change immediately (ux)
-      const i = interventionList.value.findIndex((o:Observation) => o.observationId === intervention.interventionId)
+      const i = interventionList.value.findIndex((i:Intervention) => i.interventionId === intervention.interventionId)
       if(i>-1) {
         interventionList.value[i] = intervention;
       }
 
       await interventionsApi.updateIntervention(props.studyId, intervention.interventionId as number, intervention)
         .then(listInterventions)
-    }catch(e) {
+    } catch(e) {
       console.error("Couldn't update opservation " + intervention.title);
     }
   }
 
-  /*async function createIntervention(newIntervention: Intervention) {
+  async function deleteIntervention(requestIntervention: Intervention) {
+    try {
+      await interventionsApi.deleteIntervention(props.studyId, requestIntervention.interventionId as number)
+        .then(listInterventions)
+    } catch (e) {
+      console.error('Cannot delete intervention ' + requestIntervention.interventionId, e)
+    }
+  }
+
+  async function createIntervention(newIntervention: Intervention) {
     try {
       await interventionsApi.addIntervention(props.studyId, newIntervention)
         .then((response: AxiosResponse) => {
@@ -86,7 +97,8 @@
       } catch(e) {
         console.error("Cannot create intervention", e);
     }
-  }   */
+  }
+
   /*
   const intervention: Intervention = {
     title: "test intervention",
@@ -100,20 +112,63 @@
       }
     ]
   }
-  createIntervention(intervention);  */
+  createIntervention(intervention);
+   */
 
-  async function deleteIntervention(requestIntervention: Intervention) {
+  async function updateIntervention(intervention: Intervention) {
     try {
-      await interventionsApi.deleteIntervention(props.studyId, requestIntervention.interventionId as number)
-        .then(listInterventions)
-    } catch (e) {
-      console.error('Cannot delete observation ' + requestIntervention.interventionId, e)
+      const i = interventionList.value.findIndex(v => v.interventionId === intervention.interventionId);
+      if(i>-1) {
+        interventionList.value[i] = intervention;
+        await interventionsApi.updateIntervention(props.studyId, intervention.interventionId as number, intervention)
+          .then(listInterventions)
+      }
+    } catch(e) {
+      console.error('Cannot update intervention: ' + intervention.interventionId, e);
     }
   }
 
-  /*function openInterventionDialog(actionResult: MoreTableActionResult) {
+  function openEditIntervetion(interventionId: number) {
+    const intervention = interventionList.value.find(i => i.interventionId === interventionId);
+    if(intervention) {
+      openInterventionDialog('Edit intervention', intervention);
+    }
+  }
+
+  function openInterventionDialog(headerText: string, intervention?: Intervention, typeText?: string) {
     console.log('openInterventionDialog')
-  }  */
+    dialog.open(InterventionDialog, {
+      data: {
+        groupStates: groupStatuses.value,
+        intervention: intervention
+      },
+      props: {
+        header: headerText,
+        style: {
+          width: '50vw',
+        },
+        breakpoints:{
+          '960px': '75vw',
+          '640px': '90vw'
+        },
+        modal: true,
+        dismissableMask: true,
+      },
+      onClose: (options) => {
+        if(options?.data) {
+          if(options.data?.interventionId) {
+            if(typeText) {
+              createIntervention(options.data as Intervention)
+            } else {
+              updateIntervention(options.data as Intervention)
+            }
+          } else {
+            createIntervention(options.data as Intervention)
+          }
+        }
+      }
+    })
+  }
 
   listInterventions();
 
@@ -130,7 +185,7 @@
       :row-actions="rowActions"
       :table-actions="tableActions"
       :sort-options="{sortField: 'title', sortOrder: -1}"
-      empty-message="No observations yet"
+      empty-message="No interventions yet"
       @onselect="openInterventionDialog($event)"
       @onaction="execute($event)"
       @onchange="changeValue($event)"
---
+++ src/components/dialog/InterventionDialog.vue
@@ -0,0 +1,154 @@
+<script setup lang="ts">
+  import {inject, ref, Ref} from 'vue';
+  import InputText from 'primevue/inputtext'
+  import Textarea from 'primevue/textarea';
+  import Button from 'primevue/button';
+  import SplitButton from 'primevue/splitbutton';
+  import Dropdown from 'primevue/dropdown';
+  import {Intervention, Observation} from '../../generated-sources/openapi';
+  import {Action} from '../../generated-sources/openapi';
+
+  const dialogRef:any = inject("dialogRef")
+  const intervention:Intervention = dialogRef.value.data?.intervention || {};
+  const groupStates = dialogRef.value.data?.groupStates || undefined;
+  const groupPlaceholder = dialogRef.value.data?.groupPlaceholder || 'Choos a group';
+
+  const title = ref(intervention.title);
+  const purpose = ref(intervention.purpose);
+  const trigger = ref(intervention.trigger);
+  const actions: Ref<Action[]> = ref(intervention.actions || [{
+    type: "acc-mobile-observation",
+    properties: {}
+  }]);
+  const actionsString: Ref<Action[]> = ref(actionsStringify(actions.value))
+  const studyGroupId = ref(intervention.studyGroupId)
+
+  const actionItems = [
+    {
+      label: "Accelerometer Mobile",
+      value: "acc-mobile-observation",
+      command: () => {
+        actions.value.push({type: 'acc-mobile-observation', properties: {}})
+      }
+    }
+  ]
+
+  function save(){
+    const returnObservation = {
+      title: title.value,
+      purpose: purpose.value,
+      trigger: trigger.value,
+      actions: actions.value,
+      studyGroupId: studyGroupId.value,
+      scheduler: intervention.schedule
+    } as Observation;
+
+    dialogRef.value.close(returnObservation);
+  }
+  function cancel() {
+    dialogRef.value.close();
+  }
+
+  function addNewAction(e: any) {
+    console.log(e);
+    //actions.value.push({type: 'acc-mobile-observation', properties: {}})
+  }
+  function deleteAction(index: number) {
+    console.log(index);
+    actions.value.splice(index, 1);
+  }
+
+  function actionsStringify(actions: Object[]) {
+    const a: Ref<Array<any>> = ref([{}])
+    if (actions) {
+      return actions.map((item: any) => (JSON.stringify(item)))
+    }
+    /*if(actions.value) {
+      actions.value.forEach((item) => {
+        a.value.push(JSON.stringify(item))
+      })
+    }*/
+
+
+  }
+
+  console.log("actionString");
+  console.log(actionsString.value)
+
+
+</script>
+
+
+<template>
+  <div class="intervention-dialog">
+    <div class="grid grid-cols-8 gap-4 items-center">
+      <div class="col-start-0 col-span-2"><h5>{{ $t('intervention') }} {{ $t('title') }}</h5></div>
+      <div class="col-start-3 col-span-6">
+        <InputText v-model="title" placeholder="Enter intervention title." style="width: 100%"></InputText>
+      </div>
+
+      <div class="col-start-0 col-span-8">
+        <h5 class="mb-2">{{ $t('purpose') }}</h5>
+        <Textarea v-model="purpose" placeholder="Enter the main purpose and intention of the study." :auto-resize="true" style="width: 100%"></Textarea>
+      </div>
+
+      <div class="col-start-0 col-span-8">
+        <h5 class="mb-2">{{$t('trigger')}}</h5>
+        <div class="col-start-0 col-span-8">
+          <h6 class="mb-1">Config(Json)</h6>
+          <Textarea v-model="trigger" placeholder="Enter the config for the trigger" :auto-resize="true" style="width: 100%"></Textarea>
+        </div>
+      </div>
+
+      <div class="col-start-0 col-span-8 grid grid-cols-9">
+        <h5 class="mb-2 col-span-7">{{$t('action')}}</h5>
+        <SplitButton @click="addNewAction($event)" class="splitButton w-full col-span-2" :key="actions.length" type="button" :label="'New Action'" :icon="'pi pi-plus'" :model='actionItems'></SplitButton>
+        <div v-for="(action, index) in actions" :key="index" class="col-start-0 col-span-9 js-action grid mb-4" >
+          <div class="mb-3">
+            <h6 class="mb-1 col-span-2 inline">Config(Json): </h6>
+            <div class="col-span-3 inline font-medium">{{action.type}}</div>
+          </div>
+          <div class="col-span-4 justify-end"></div>
+          <Textarea v-model="actions[index].properties" class="col-span-9" placeholder="Enter the config for the action" :auto-resize="true" style="width: 100%" />
+          <div class="buttons justify-end mt-2 col-span-9">
+             <Button :icon="'pi pi-trash'" @click="deleteAction(index)"/>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-start-0 col-span-8">
+        <Dropdown v-model="studyGroupId" :options="groupStates" option-label="label" option-value="value" :placeholder="$t(groupPlaceholder)">
+          <template #option="optionProps">
+            <div class="p-dropdown-car-option">
+              <span>{{optionProps.option.label}}</span>
+            </div>
+          </template>
+        </Dropdown>
+      </div>
+
+      <div class=" col-start-0 col-span-8 buttons text-right mt-8 justify-end">
+        <Button class="p-button-secondary" @click="cancel()">Cancel</Button>
+        <Button @click="save()">Save</Button>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<style lang="postcss">
+.intervention-dialog {
+  .buttons {
+    button {
+      margin-left: 10px;
+    }
+  }
+  .splitButton {
+    background-color: var(--primary-color);
+    color: white;
+    padding: 0 6px;
+    justify-content: center;
+    align-items: center;
+  }
+}
+
+</style>
