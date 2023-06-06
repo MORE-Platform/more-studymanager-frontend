7bf76d1a4d4cf1216177aac80b4e566c3e5ca09f
iaigner <isabella.aigner@redlink.at>
2023-06-06 12:20:59 +0200

    MORE-925: Extend Study Dialog with contact data (dummy data)

--- src/components/dialog/StudyDialog.vue
+++ src/components/dialog/StudyDialog.vue
@@ -25,6 +25,13 @@
     participantInfo: study.participantInfo,
   }) as Ref<Study>;
 
+  const contactInfo: Ref<any> = ref({
+    institute: 'Ludwig Bolzmann Institut',
+    contactPerson: 'Dr. Max Mustermann',
+    contactEmail: 'max.mustermann@bolzmann.at',
+    contactTel: undefined,
+  });
+
   const start = ref(
     study
       ? study.plannedStart
@@ -206,6 +213,54 @@
           :auto-resize="true"
           style="width: 100%"
         ></Textarea>
+      </div>
+      <div class="col-span-6 mb-4 mt-2">
+        <h5 class="mb-2 font-bold">
+          {{ $t('study.dialog.label.contactInfo') }}
+        </h5>
+        <div class="mb-3">{{ $t('study.dialog.description.contactData') }}</div>
+        <div class="grid grid-cols-6 gap-4">
+          <div class="col-span-3">
+            <h6 class="mb-1 font-medium">
+              {{ $t('study.dialog.label.institute') }}
+            </h6>
+            <InputText
+              v-model="contactInfo.institute"
+              class="w-full"
+              :placeholder="t('study.placeholder.institute')"
+            />
+          </div>
+          <div class="col-span-3">
+            <h6 class="mb-1 font-medium">
+              {{ $t('study.dialog.label.contactPerson') }}*
+            </h6>
+            <InputText
+              v-model="contactInfo.contactPerson"
+              class="w-full"
+              :placeholder="t('study.placeholder.contactInfo')"
+            />
+          </div>
+          <div class="col-span-3">
+            <h6 class="mb-1 font-medium">
+              {{ $t('study.dialog.label.contactEmail') }}*
+            </h6>
+            <InputText
+              v-model="contactInfo.contactEmail"
+              class="w-full"
+              :placeholder="t('study.placeholder.contactEmail')"
+            />
+          </div>
+          <div class="col-span-3">
+            <h6 class="mb-1 font-medium">
+              {{ $t('study.dialog.label.contactTel') }}
+            </h6>
+            <InputText
+              v-model="contactInfo.contactTel"
+              class="w-full"
+              :placeholder="t('study.placeholder.contactTel')"
+            />
+          </div>
+        </div>
       </div>
       <div class="buttons col-start-0 col-span-6 mt-1 justify-end text-right">
         <Button class="btn-gray" @click="cancel()">{{
