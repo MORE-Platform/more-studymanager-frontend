e73d73f371a9e2e97279d2ebb0908927f4b56f40
iaigner <isabella.aigner@redlink.at>
2023-07-27 12:16:47 +0200

    MORE-1039: i18n fix

--- src/models/InputModels.ts
+++ src/models/InputModels.ts
@@ -76,7 +76,7 @@ export abstract class Property<T> {
       this.name = this.id;
     }
     if (!this.description) {
-      this.description = `Please enter a ${this.id} value.`;
+      this.description = `inputModel.enterValue`;
     }
   }
 
