819016b904ac57bd807c1aae233a89570d656a39
iaigner <isabella.aigner@redlink.at>
2023-08-16 10:25:09 +0200

    MORE-1053: Change cron schedule example format into datatable to improve readability

--- src/components/subComponents/CronScheduleExamples.vue
+++ src/components/subComponents/CronScheduleExamples.vue
@@ -1,73 +1,117 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+  import DataTable from 'primevue/datatable';
+  import Column from 'primevue/column';
+  import { useI18n } from 'vue-i18n';
+  const { t } = useI18n();
+
+  const tableColumns = [
+    {
+      field: 'title',
+      header: 'Title',
+    },
+    {
+      field: 'minutes',
+      header: t('cronSchedule.labels.minutes'),
+    },
+    {
+      field: 'hours',
+      header: t('cronSchedule.labels.hours'),
+    },
+    {
+      field: 'dayOfMonth',
+      header: t('cronSchedule.labels.dayOfMonth'),
+    },
+    {
+      field: 'months',
+      header: t('cronSchedule.labels.months'),
+    },
+    {
+      field: 'dayOfWeek',
+      header: t('cronSchedule.labels.dayOfWeek'),
+    },
+  ];
+
+  const tableRows = [
+    {
+      title: t('cronSchedule.example.titles.everyMinute'),
+      minutes: '*',
+      hours: '*',
+      dayOfMonth: '?',
+      months: '*',
+      dayOfWeek: '*',
+    },
+    {
+      title: t('cronSchedule.example.titles.hourly'),
+      minutes: '30',
+      hours: '*',
+      dayOfMonth: '?',
+      months: '*',
+      dayOfWeek: '*',
+    },
+    {
+      title: t('cronSchedule.example.titles.daily'),
+      minutes: '0',
+      hours: '12',
+      dayOfMonth: '?',
+      months: '*',
+      dayOfWeek: '*',
+    },
+    {
+      title: t('cronSchedule.example.titles.specificDayOfMonth'),
+      minutes: '*',
+      hours: '*',
+      dayOfMonth: '4',
+      months: '*',
+      dayOfWeek: '*',
+    },
+    {
+      title: t('cronSchedule.example.titles.specificMonth'),
+      minutes: '0',
+      hours: '8',
+      dayOfMonth: '*',
+      months: '3',
+      dayOfWeek: '?',
+    },
+    {
+      title: t('cronSchedule.example.titles.everyFiveMinutes'),
+      minutes: '0/5',
+      hours: '*',
+      dayOfMonth: '?',
+      months: '*',
+      dayOfWeek: '*',
+    },
+    {
+      title: t('cronSchedule.example.titles.everyMinuteInSetTime'),
+      minutes: '0-10',
+      hours: '*',
+      dayOfMonth: '?',
+      months: '*',
+      dayOfWeek: '*',
+    },
+    {
+      title: t('cronSchedule.example.titles.specificHourRange'),
+      minutes: '30',
+      hours: '8-10',
+      dayOfMonth: '?',
+      months: '*',
+      dayOfWeek: '*',
+    },
+  ];
+</script>
 
 <template>
-  <div class="cron-schedule-examples">
+  <div class="cron-schedule-examples mt-5">
     <h6 class="mb-1 font-medium">{{ $t('cronSchedule.example.title') }}</h6>
-    <div class="examples grid grid-cols-3 gap-2 lg:grid-cols-5">
-      <div>
-        <div class="color-primary">
-          {{ $t('cronSchedule.example.titles.everyMinute') }}
-        </div>
-        <div class="font-medium">
-          {{ $t('cronSchedule.example.values.everyMinute') }}
-        </div>
-      </div>
-      <div>
-        <div class="color-primary">
-          {{ $t('cronSchedule.example.titles.hourly') }}
-        </div>
-        <div class="font-medium">
-          {{ $t('cronSchedule.example.values.hourly') }}
-        </div>
-      </div>
-      <div>
-        <div class="color-primary">
-          {{ $t('cronSchedule.example.titles.daily') }}
-        </div>
-        <div class="font-medium">
-          {{ $t('cronSchedule.example.values.daily') }}
-        </div>
-      </div>
-      <div>
-        <div class="color-primary">
-          {{ $t('cronSchedule.example.titles.specificDayOfMonth') }}
-        </div>
-        <div class="font-medium">
-          {{ $t('cronSchedule.example.values.specificDayOfMonth') }}
-        </div>
-      </div>
-      <div>
-        <div class="color-primary">
-          {{ $t('cronSchedule.example.titles.specificMonth') }}
-        </div>
-        <div class="font-medium">
-          {{ $t('cronSchedule.example.values.specificMonth') }}
-        </div>
-      </div>
-      <div>
-        <div class="color-primary">
-          {{ $t('cronSchedule.example.titles.everyFiveMinutes') }}
-        </div>
-        <div class="font-medium">
-          {{ $t('cronSchedule.example.values.everyFiveMinutes') }}
-        </div>
-      </div>
-      <div>
-        <div class="color-primary">
-          {{ $t('cronSchedule.example.titles.specificHourRange') }}
-        </div>
-        <div class="font-medium">
-          {{ $t('cronSchedule.example.values.specificHourRange') }}
-        </div>
-      </div>
-      <div class="col-span-2">
-        <div class="color-primary">
-          {{ $t('cronSchedule.example.titles.everyMinuteInSetTime') }}
-        </div>
-        <div class="font-medium">
-          {{ $t('cronSchedule.example.values.everyMinuteInSetTime') }}
-        </div>
-      </div>
+
+    <div class="examples">
+      <DataTable :value="tableRows">
+        <Column
+          v-for="col in tableColumns"
+          :key="col.field"
+          :field="col.field"
+          :header="col.header"
+        />
+      </DataTable>
     </div>
   </div>
 </template>
@@ -75,8 +119,31 @@
 <style scoped lang="postcss">
   .cron-schedule-examples .examples {
     border: 1px solid var(--surface-50);
-    padding: 1rem 0.5rem;
+    padding: 1rem;
     border-radius: 6px;
     background-color: var(--surface-50);
+  }
+  :deep(.p-datatable-wrapper tr) {
+    th {
+      padding-top: 0;
+      padding-bottom: 7px;
+
+      .p-column-title {
+        width: 100%;
+      }
+    }
+    td,
+    th {
+      text-align: center;
+
+      &:first-of-type {
+        font-weight: 500;
+        text-align: left;
+      }
+    }
+
+    td:first-of-type {
+      color: var(--primary-color);
+    }
   }
 </style>
