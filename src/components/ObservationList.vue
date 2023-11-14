b98a2c79be001916a30c9f5a5ef10e1045205d35
iaigner <isabella.aigner@redlink.at>
2023-11-14 10:39:37 +0100

    MORE2-10: add time to relative preview in observation list

--- src/components/ObservationList.vue
+++ src/components/ObservationList.vue
@@ -282,13 +282,14 @@
               schedule.dtstart.offset?.unit
               ? `${t(
                   `scheduler.preview.unit.${schedule.dtstart.offset.unit}`
-                )} ${schedule.dtstart.offset.value} `
+                )} ${schedule.dtstart.offset.value}, ${schedule.dtstart.time}`
               : undefined;
           case 'dtend':
             return schedule.dtend.offset?.value && schedule.dtend.offset?.unit
               ? `${t(`scheduler.preview.unit.${schedule.dtend.offset.unit}`)} ${
                   schedule.dtend.offset.value
-                } `
+                }, ${schedule.dtend.time
+              } `
               : undefined;
           default:
             return undefined;
