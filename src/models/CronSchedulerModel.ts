e2effc6bd4afa6f9b125d233c208226242ba1b4a
Alireza Fatehi <107924035+alireza-dhp@users.noreply.github.com>
2023-11-14 15:16:25 +0100

    add license text for all *.vue and *.ts files

--- src/models/CronSchedulerModel.ts
+++ src/models/CronSchedulerModel.ts
@@ -1,3 +1,11 @@
+/*
+ * Copyright LBI-DHP and/or licensed to LBI-DHP under one or more
+ * contributor license agreements (LBI-DHP: Ludwig Boltzmann Institute
+ * for Digital Health and Prevention -- A research institute of the
+ * Ludwig Boltzmann Gesellschaft, Oesterreichische Vereinigung zur
+ * Foerderung der wissenschaftlichen Forschung).
+ * Licensed under the Elastic License 2.0.
+ */
 export interface TriggerSchedule {
   seconds: string;
   minutes: string;
