d40aaba22ca48ec462ee5b501ce5a282e16177aa
Alireza Fatehi <alireza.fatehi@dhp.lbg.ac.at>
2023-11-15 10:23:14 +0100

    dashboard.vue test commit

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -7,47 +7,47 @@
  * Licensed under the Elastic License 2.0.
  */
 <script setup lang="ts">
-  import StudyList from '../components/StudyList.vue';
-  import User from '../components/User.vue';
-  import { useDialog } from 'primevue/usedialog';
-  import InfoDialog from '../components/dialog/InfoDialog.vue';
-  import { useUserStore } from '../stores/userStore';
-  import { useI18n } from 'vue-i18n';
+import StudyList from '../components/StudyList.vue';
+import User from '../components/User.vue';
+import { useDialog } from 'primevue/usedialog';
+import InfoDialog from '../components/dialog/InfoDialog.vue';
+import { useUserStore } from '../stores/userStore';
+import { useI18n } from 'vue-i18n';
 
-  const userStore = useUserStore();
-  const dialog = useDialog();
-  const { t } = useI18n();
+const userStore = useUserStore();
+const dialog = useDialog();
+const { t } = useI18n();
 
-  userStore.getUser().then(() => openWelcomeMessage());
+userStore.getUser().then(() => openWelcomeMessage());
 
-  function openWelcomeMessage() {
-    const storageItem = localStorage.getItem('welcomeMsg');
-    if (userStore.user?.uid && !storageItem) {
-      dialog.open(InfoDialog, {
-        data: {
-          message: t('welcomeMessage', {
-            userName: userStore.userName,
-            userInstitution: userStore.userInstitution,
-          }),
+function openWelcomeMessage() {
+  const storageItem = localStorage.getItem('welcomeMsg');
+  if (userStore.user?.uid && !storageItem) {
+    dialog.open(InfoDialog, {
+      data: {
+        message: t('welcomeMessage', {
+          userName: userStore.userName,
+          userInstitution: userStore.userInstitution,
+        }),
+      },
+      props: {
+        header: 'Welcome to More',
+        style: {
+          width: '50vw',
         },
-        props: {
-          header: 'Welcome to More',
-          style: {
-            width: '50vw',
-          },
-          breakpoints: {
-            '960px': '75vw',
-            '640px': '90vw',
-          },
-          modal: true,
-          draggable: false,
+        breakpoints: {
+          '960px': '75vw',
+          '640px': '90vw',
         },
-        onClose: () => {
-          localStorage.setItem('welcomeMsg', 'hide');
-        },
-      });
-    }
+        modal: true,
+        draggable: false,
+      },
+      onClose: () => {
+        localStorage.setItem('welcomeMsg', 'hide');
+      },
+    });
   }
+}
 </script>
 
 <template>
