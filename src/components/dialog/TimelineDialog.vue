042edfa0707aba3e544b94fdf0b47a0c03b4a454
bnitsch <>
2024-05-29 11:42:01 +0200

    #376: Add more events in FE, because we receive more information from the BE.
    * Add "participantSignup" as event.
    * Add "studyDuration" as allDay event.
    * Extend non date range events (intervention and participantSignup) from internal +15min to +30min to make it easier to click on.
    * Only show necessary information in the event, depending on the event type (observation, intervention, participantSignup)
    * Fix issue in dialog where we assign a class instead of checking if it is the class = vs ===.

--- src/components/dialog/TimelineDialog.vue
+++ src/components/dialog/TimelineDialog.vue
@@ -8,10 +8,10 @@
   const dialogRef: any = inject('dialogRef');
   const event: Event = dialogRef.value.data.event;
   const startDate = `${t('global.labels.start')}: ${dayjs(event.start).format(
-    'DD/MM/YYYY'
+    'DD/MM/YYYY',
   )}, ${dayjs(event.start).format('HH:mm')}`;
   const endDate = `${t('global.labels.end')}: ${dayjs(event.end).format(
-    'DD/MM/YYYY'
+    'DD/MM/YYYY',
   )}, ${dayjs(event.end).format('HH:mm')}`;
   const scheduleType = event.cScheduleType;
   const typeTranslation = event.cTypeTranslation;
@@ -22,11 +22,11 @@
 <template>
   <div class="dialog">
     <p>{{ startDate }}</p>
-    <p v-if="(event.class = 'observation')">{{ endDate }}</p>
+    <p v-if="event.class === 'observation'">{{ endDate }}</p>
     <p>{{ scheduleType }}</p>
     <p>{{ typeTranslation }}</p>
-    <p v-if="purpose">{{ t('study.props.purpose') + ':' + purpose }}</p>
-    <p v-if="(event.class = 'observation')">
+    <p v-if="purpose">{{ t('study.props.purpose') + ': ' + purpose }}</p>
+    <p v-if="event.class === 'observation'">
       {{
         hidden
           ? t('observation.props.hidden.true')
