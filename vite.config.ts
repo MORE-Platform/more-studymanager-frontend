459b529b08234af5d3677ee8084fa48cd9653878
Jakob Frank <jakob.frank@redlink.at>
2024-06-05 23:01:56 +0200

    #463: Fixed ENV-Handling during build

--- vite.config.ts
+++ vite.config.ts
@@ -11,8 +11,8 @@ export default defineConfig({
   define: {
     __APP_VERSION__: JSON.stringify(process.env.npm_package_version),
     __BUILD_DATE__: JSON.stringify(new Date().toISOString()),
-    __BUILD_BRANCH__: 'import.meta.env.VITE_GIT_BRANCH',
-    __BUILD_REVISION__: 'import.meta.env.VITE_GIT_REVISION',
+    __BUILD_BRANCH__: JSON.stringify(process.env.VITE_GIT_BRANCH),
+    __BUILD_REVISION__: JSON.stringify(process.env.VITE_GIT_REVISION),
   },
   server: {
     port: 3000,
