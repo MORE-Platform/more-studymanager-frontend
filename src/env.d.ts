4427b93c0748292fa29b81f71618acdfcf11e12c
Jakob Frank <jakob.frank@redlink.at>
2024-06-05 22:50:17 +0200

    Recover "#463: Show build-info in the footer"
    This reverts commit 96dc94fbae1c56e35d5c2377821d44ad6285e2ea.

--- src/env.d.ts
+++ src/env.d.ts
@@ -8,6 +8,10 @@
  */
 /// <reference types="vite/client" />
 
+declare const __APP_VERSION__: string;
+declare const __BUILD_DATE__: string;
+declare const __BUILD_BRANCH__: string;
+declare const __BUILD_REVISION__: string;
 declare module '*.vue' {
   import { DefineComponent } from 'vue';
   // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/ban-types
