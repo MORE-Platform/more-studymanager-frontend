c6228a073ae3be4b50ce75b96a9894a3d14c3bfa
bnitsch <bernhard.nitsch@redlink.at>
2024-06-24 08:30:18 +0200

    #470: Timeline refactoring
    * Introduce TS interfaces for Timeline.
    * Do not save custom data on vue-cal Event object (only cId).
    * Use mapper and EventDetail object for more detailed information on e.g. dialog.
    * Avoid using class names in conditions for display information.

---
+++ src/models/Timeline.ts
@@ -0,0 +1,37 @@
+/*
+ Copyright LBI-DHP and/or licensed to LBI-DHP under one or more
+ contributor license agreements (LBI-DHP: Ludwig Boltzmann Institute
+ for Digital Health and Prevention -- A research institute of the
+ Ludwig Boltzmann Gesellschaft, Oesterreichische Vereinigung zur
+ Foerderung der wissenschaftlichen Forschung).
+ Licensed under the Elastic License 2.0.
+ */
+import { Event } from 'vue-cal';
+
+export interface DropdownOption {
+  label: string;
+  value: string | undefined;
+}
+
+export interface GroupOption {
+  label: string;
+  items: Array<DropdownOption>;
+}
+
+export interface EventDetail extends Event {
+  isHidden?: boolean;
+  scheduleType?: string;
+  typeTranslation?: string;
+  type?: string;
+  purpose?: string;
+  options: Array<number>;
+}
+
+export enum EventOptions {
+  showEyeIcon,
+  ignoreFilter, // always show in the calendar
+  showDialogInformation, // clickable to open a dialog popup
+  showEndDateInDialog,
+  showStartTime, // show HH:mm in the calendar event
+  showEndTime, // show HH:mm in the calendar event
+}
