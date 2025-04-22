d3e12aee906e5a9375d50dd968210874a8862be8
Manuel T. Schrempf <manuel.schrempf@redlink.at>
2025-04-22 08:31:21 +0200

    #559: Improve naming of constants, consistent name + upper case as usual

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,3 @@
-export const minutesInDay = 1440;
-export const minutesInHour = 60;
 export const PARTICIPANT_COUNTS = [1, 3, 10, 25, 50];
+export const ONE_DAY_IN_MINUTES = 1440;
+export const ONE_HOUR_IN_MINUTES = 60;
